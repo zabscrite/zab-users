a874580311bc5f64a5ca6ac60f7283bb9ab03cd9
pool <loop.edward@gmail.com>
2022-08-18 20:52:13 +0800

    Initial configuration for JWT

---
+++ src/main/java/com/zab/zabusers/JwtUserDetailsService.java
@@ -0,0 +1,55 @@
+package com.zab.zabusers;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // TODO: This is still a stub
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return Collections.EMPTY_LIST;
+            }
+
+            @Override
+            public String getPassword() {
+                return "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6";
+            }
+
+            @Override
+            public String getUsername() {
+                return "pool.golez@example.com";
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return true;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+        };
+    }
+}
