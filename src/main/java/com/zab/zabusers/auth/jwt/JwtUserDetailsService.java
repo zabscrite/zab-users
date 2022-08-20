c023918fa1a8ccdc5f0557d0caeb308a3b38dc8e
pool <loop.edward@gmail.com>
2022-08-20 11:26:46 +0800

    Update stub in userDetailsService to retrieve user from database

--- src/main/java/com/zab/zabusers/auth/jwt/JwtUserDetailsService.java
+++ src/main/java/com/zab/zabusers/auth/jwt/JwtUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.zab.zabusers.auth.jwt;
 
+import com.zab.zabusers.user.User;
+import com.zab.zabusers.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,9 +15,20 @@ import java.util.Collections;
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO: This is still a stub
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return null;
+        }
+
+        return mapUserDetails(user);
+    }
+
+    private UserDetails mapUserDetails(User user) {
         return new UserDetails() {
             @Override
             public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -23,12 +37,12 @@ public class JwtUserDetailsService implements UserDetailsService {
 
             @Override
             public String getPassword() {
-                return "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6";
+                return user.getPassword();
             }
 
             @Override
             public String getUsername() {
-                return "pool.golez@example.com";
+                return user.getUsername();
             }
 
             @Override
