9699df4d67937ff40bc16f986c8b12ed9017549e
pool <loop.edward@gmail.com>
2022-08-22 06:04:45 +0800

    Validate LoginRequest parameter to pass controller architectural tests

--- src/main/java/com/zab/zabusers/auth/controller/LoginRequest.java
+++ src/main/java/com/zab/zabusers/auth/controller/LoginRequest.java
@@ -3,11 +3,15 @@ package com.zab.zabusers.auth.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class LoginRequest {
 
+    @NotBlank(message = "Username is required")
     private String username;
 
+    @NotBlank(message = "Password is required")
     private String password;
 }
