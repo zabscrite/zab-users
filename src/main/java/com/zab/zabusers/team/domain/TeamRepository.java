437d3a1ac623e88d05aeb3c5e78150827472ab59
pool <loop.edward@gmail.com>
2022-08-27 10:51:30 +0800

    Add repository method to find team by owner

--- src/main/java/com/zab/zabusers/team/domain/TeamRepository.java
+++ src/main/java/com/zab/zabusers/team/domain/TeamRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TeamRepository extends CrudRepository<Team, Long> {
 
     List<Team> findAll();
+
+    Optional<Team> findByOwner(User user);
+
 }
