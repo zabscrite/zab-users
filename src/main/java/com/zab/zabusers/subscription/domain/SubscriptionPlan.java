a8dbaba409d8582500d3c33dd934527e1e5d3f0f
pool <loop.edward@gmail.com>
2022-08-27 12:46:53 +0800

    Configure @EqualsAndHashCode on SubscriptionPlan and Team

--- src/main/java/com/zab/zabusers/subscription/domain/SubscriptionPlan.java
+++ src/main/java/com/zab/zabusers/subscription/domain/SubscriptionPlan.java
@@ -1,19 +1,29 @@
 package com.zab.zabusers.subscription.domain;
 
 import com.zab.zabusers.team.domain.Team;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "subscription_plans")
 @EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class SubscriptionPlan {
 
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "subscription_plans_id_seq")
+    @EqualsAndHashCode.Include
     private Long id;
 
     @Getter
