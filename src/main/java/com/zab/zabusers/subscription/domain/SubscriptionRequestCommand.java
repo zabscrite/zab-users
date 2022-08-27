1c9e70424d2ee64bf96ddd8e43f13d8874240be7
pool <loop.edward@gmail.com>
2022-08-27 12:47:21 +0800

    Add team field on subscription command

--- src/main/java/com/zab/zabusers/subscription/domain/SubscriptionRequestCommand.java
+++ src/main/java/com/zab/zabusers/subscription/domain/SubscriptionRequestCommand.java
@@ -1,11 +1,14 @@
 package com.zab.zabusers.subscription.domain;
 
+import com.zab.zabusers.team.domain.Team;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SubscriptionRequestCommand {
+
+    private Team team;
 
     private Customer customer;
 
