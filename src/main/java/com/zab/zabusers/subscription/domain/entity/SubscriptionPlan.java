8ed884bd019e645f9a89b0bd83c8e1ec2afd543c
pool <loop.edward@gmail.com>
2022-08-27 20:25:44 +0800

    Add status field to SubscriptionPlan

--- src/main/java/com/zab/zabusers/subscription/domain/entity/SubscriptionPlan.java
+++ src/main/java/com/zab/zabusers/subscription/domain/entity/SubscriptionPlan.java
@@ -15,6 +15,8 @@ import java.util.Date;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class SubscriptionPlan {
 
+    public enum Status {DRAFT, ACTIVE, INACTIVE, ARCHIVED}
+
     @Id
     @Getter
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "subscription_plans_id_seq")
@@ -38,6 +40,11 @@ public class SubscriptionPlan {
             @AttributeOverride(name = "unit", column = @Column(name = "duration_unit"))
     })
     private SubscriptionPlanDuration duration;
+
+    @Getter
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.DRAFT;
 
     public Date computeExpiration(Date startDate) {
         SubscriptionPlanDuration duration = getDuration();
