b9bca02c9880f1b88424d8106ded9c2b0ec02f28
pool <loop.edward@gmail.com>
2022-08-28 15:24:24 +0800

    Create method to query expired subscriptions

--- src/main/java/com/zab/zabusers/subscription/domain/repository/SubscriptionRepository.java
+++ src/main/java/com/zab/zabusers/subscription/domain/repository/SubscriptionRepository.java
@@ -1,9 +1,16 @@
 package com.zab.zabusers.subscription.domain.repository;
 
 import com.zab.zabusers.subscription.domain.entity.Subscription;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface SubscriptionRepository extends CrudRepository<Subscription, Long> {
+
+    @Query("SELECT s FROM Subscription s " +
+            "WHERE s.status = 'OPEN' AND s.expirationDate <= now()")
+    Set<Subscription> findAllExpired();
 }
